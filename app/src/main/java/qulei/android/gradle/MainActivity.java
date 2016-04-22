c2906b76a520746146796df076dab3c11a18e434
qulei <qulei@che08.com>
2016-04-22 16:12:13 +0800

    添加遗漏文件

---
+++ app/src/main/java/qulei/android/gradle/MainActivity.java
@@ -0,0 +1,45 @@
+package qulei.android.gradle;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.tv);
+
+        tv.setText("versionCode : " + getVersionCode(this) + "\n"
+                + "versionName : " + getVersionName(this));
+
+    }
+
+
+    public static String getVersionName(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            return "";
+        }
+    }
+
+    public static int getVersionCode(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return info.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            return 0;
+        }
+    }
+
+
+
+}
